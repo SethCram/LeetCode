9e792d332cbf52d32b82fe53630e74036d481a55
sethhi2 <sethcram@gmail.com>
2022-08-27 20:26:39 -0600

    Moved Files into folders by lang

---
+++ C#/SortedArrSqrs.cs
@@ -0,0 +1,17 @@
+namespace LeetCode
+{
+    public class SortedArrSqrs
+    {
+
+        public int[] SortedSquares(int[] nums) 
+        {
+            //declare new arr of same size as incoming one
+            int[] sqrdArr = new int[nums.Length];
+
+            
+
+            return sqrdArr;
+        }
+
+    }
+}
