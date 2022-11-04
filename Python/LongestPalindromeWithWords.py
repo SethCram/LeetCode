0189f4cef65f8174db7bea08b48191a6a023982d
sethhi2 <sethcram@gmail.com>
2022-11-03 20:21:48 -0600

    works properly but takes too long

--- Python/LongestPalindromeWithWords.py
+++ Python/LongestPalindromeWithWords.py
@@ -3,11 +3,9 @@ class Solution:
         
         comboStrs = words[:]
         
-        print(words[0])
-        
         words2D  = [[twoLetterWord] for twoLetterWord in words]
         
-        self.CreateAllStrs(words2D, words2D, comboStrs)  
+        self.CreateAllStrs(words2D, words, words2D, comboStrs)  
         
         #walk thru combo strs
         for i in range(1, len(comboStrs)+1):
@@ -24,17 +22,19 @@ class Solution:
         
         return 0
     
-    def CreateAllStrs(self, currWordList, twoLetterWords, comboStrs):
+    def CreateAllStrs(self, currWordList, twoLetterWords, twoLetterWordsList, comboStrs):
         
-        if len(currWordList[-1]) == len(twoLetterWords) * 2:
+        if len(currWordList[-1]) == len(twoLetterWords):
             return
         
         nextWordsList = []
-        
+
         for currWord in currWordList:
-            for twoLetterWord in twoLetterWords:
-                if "".join(twoLetterWord) not in currWord: #doesn't work bc doesn't account for duplicate 2 letter word in words
-                    nextWordsList.append( currWord + twoLetterWord )
+            for i in range(len(twoLetterWords)):
+                
+                #if "".join(twoLetterWord) not in currWord: #doesn't work bc doesn't account for duplicate 2 letter word in words
+                if currWord.count(twoLetterWords[i]) != twoLetterWords.count(twoLetterWords[i]): #if haven't reached num of duplicated within currWord
+                    nextWordsList.append( currWord + twoLetterWordsList[i] )
                     
                     comboStrs.append( "".join(nextWordsList[-1]) )
              
@@ -43,10 +43,10 @@ class Solution:
                     
         #comboStrs.append(nextWordsList[:])
         
-        self.CreateAllStrs(nextWordsList, twoLetterWords, comboStrs)
+        self.CreateAllStrs(nextWordsList, twoLetterWords, twoLetterWordsList, comboStrs)
             
 sol = Solution()
 
-rslt = sol.longestPalindrome(words=["ab","ty","yt","lc","cl","ab"]) #["lc","cl","gg"])
+rslt = sol.longestPalindrome(words= ["ab","ty","yt","lc","cl","ab"]) #["lc","cl","gg"])
 
 print( f"Longest palindrome's length = {rslt}")
