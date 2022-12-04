f8386c92121195cc6d3cc28c6f3f5518efb40713
sethhi2 <sethcram@gmail.com>
2022-12-03 21:02:21 -0800

    solution found but not well understood

--- Python/DecodeString.py
+++ Python/DecodeString.py
@@ -1,19 +1,44 @@
+from collections import deque
+
 class Solution:
+    """
+    
+    """
     def decodeString(self, s: str) -> str:
-        
-        iterationsStr = ""
-        
-        for i,sLetter in enumerate(s):
-            
-            if sLetter.isnumeric():
-                iterationsStr += sLetter
-            elif sLetter == '[':
-                substrStartIndex = i + 1
-                
-            elif sLetter == ']':
-                
-                substrEndIndex = i - 1
-                
-                
-                
-                iterationsStr = ""
+        """Stack approach to preserve ordering of substr and desired duplicate number.
+        Speed: O(N)
+        Space: O(N) bc scales linearly with larger s, but will likely never reach length of s
+    
+        Args:
+            s (str): _description_
+
+        Returns:
+            str: _description_
+        """
+        stack = deque(); curNum = 0; curString = ''
+        for c in s:
+            #if start of new substring
+            if c == '[':
+                #add substring
+                stack.append(curString)
+                #add number of substrs desired
+                stack.append(curNum)
+                #reset to find next substr
+                curString = ''
+                #reset to find next num
+                curNum = 0 
+            #if end of new substring
+            elif c == ']':
+                #retrieve number and substr
+                num = stack.pop()
+                prevString = stack.pop()
+                #create new str using previous str and duplicates of curr substr
+                curString = prevString + num*curString
+            #if digit
+            elif c.isdigit():
+                #moves pre-existing number left a digit and adds new number
+                curNum = curNum*10 + int(c)
+            #if char but not a bracket (must be a letter)
+            else:
+                curString += c
+        return curString
